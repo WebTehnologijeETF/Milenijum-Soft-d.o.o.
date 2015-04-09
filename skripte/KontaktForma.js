f5ec76a4a96e8d7308b7f0dd02b00e1ab67e4c24
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-04-09 13:10:36 +0200

    poslagao fajlove i zavrsio spiralu dva

---
+++ skripte/KontaktForma.js
@@ -0,0 +1,64 @@
+var sveValidno = true;
+
+$(document).ready(function(){
+    $("#posalji").click(function(){
+        sveValidno = true;
+
+
+        var ime = document.getElementById("ime");
+        var poruka = document.getElementById("poruka");
+        var mail = document.getElementById("mail");
+        var posalji = document.getElementById("posalji");
+
+        // Validavija imena (ovo je validacija regexom)
+        if (ime.value.length == 0 ) {
+            ime.style.backgroundColor="red";
+            document.getElementById("errorIme").style.display="block";
+            document.getElementById("errorIme").innerHTML="Ime ne smije ostati prazno.";
+            sveValidno = false;
+        }
+        else if (!ime.value.match(/^[a-zA-Z ČčĆćŽžŠšĐđ]+$/)) {
+            ime.style.backgroundColor="red";
+            document.getElementById("errorIme").style.display="block";
+            document.getElementById("errorIme").innerHTML="Ime mora sadrzavati samo slova.";
+            sveValidno = false;
+        }
+        else {
+            ime.style.backgroundColor="lightgreen";
+            document.getElementById("errorIme").style.display="none";
+        }
+
+        // Validacija maila
+        if (mail.value.length == 0 ) {
+            mail.style.backgroundColor="red";
+            document.getElementById("errorMail").style.display="block";
+            document.getElementById("errorMail").innerHTML="Mail ne smije ostati prazan.";
+            sveValidno = false;
+        }
+        else if (!mail.value.match(/^[a-z0-9_]+@[a-z]+\.[a-z][a-z]+$/)) {
+            mail.style.backgroundColor="red";
+            document.getElementById("errorMail").style.display="block";
+            document.getElementById("errorMail").innerHTML="Mail mora biti u tacnom formatu.";
+            sveValidno = false;
+        }
+        else {
+            mail.style.backgroundColor="lightgreen";
+            document.getElementById("errorMail").style.display="none";
+        }
+
+        // Validacija poruke (ovo je jedna validacija bez regexa)
+        if (poruka.value.length == 0) {
+            poruka.style.backgroundColor="red";
+            document.getElementById("errorPoruka").style.display="block";
+            sveValidno = false;
+        }
+        else {
+            poruka.style.backgroundColor="lightgreen";
+            document.getElementById("errorPoruka").style.display="none";
+        }
+
+        if (true == sveValidno) {
+            posalji.type="submit";
+        }
+    });
+});
---
+++ skripte/podmeni.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#proizvodi").click(function () {
+        if ($("#pomocniDiv").width() == 0) {
+            $("#pomocniDiv").animate({'width': 90}, "slow");
+            document.getElementById("proizvodi").style.backgroundColor = "#0193B7";
+            document.getElementById("pomocniDivDva").style.display = "block";
+        }
+        else {
+            $("#pomocniDiv").animate({'width': 0}, "slow");
+            document.getElementById("proizvodi").style.backgroundColor = "transparent";
+            document.getElementById("pomocniDivDva").style.display = "none";
+        }
+        $("#podmeni").slideToggle("slow");
+    });
+    //Postavi poziciju podmenija
+    var podmeni = document.getElementById("podmeni");
+    var sakriveniDiv = document.getElementById("pomocniDiv");
+    var sakriveniDivDva = document.getElementById("pomocniDivDva");
+    var proizvodi = document.getElementById("proizvodi");
+    var pozicijaProizvodi = proizvodi.getBoundingClientRect();
+    sakriveniDiv.style.left = pozicijaProizvodi.left + 95 + "px";
+    sakriveniDiv.style.top = pozicijaProizvodi.top + "px";
+    sakriveniDivDva.style.left = pozicijaProizvodi.left + "px";
+    sakriveniDivDva.style.top = pozicijaProizvodi.top + 35 + "px";
+    podmeni.style.left = (pozicijaProizvodi.left - 25) + "px";
+    podmeni.style.top = pozicijaProizvodi.top + 40 + "px";
+});
