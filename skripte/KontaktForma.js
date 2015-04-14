5e63a54697e2bbd3f491c3b52960561ad27b898c
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-04-14 21:04:17 +0200

    Dodao onblur evente za lijepu validaciju

--- skripte/KontaktForma.js
+++ skripte/KontaktForma.js
@@ -4,58 +4,11 @@ $(document).ready(function(){
     $("#posalji").click(function(){
         sveValidno = true;
 
+        validirajIme();
+        validirajMail();
+        validirajPoruku();
 
-        var ime = document.getElementById("ime");
-        var poruka = document.getElementById("poruka");
-        var mail = document.getElementById("mail");
         var posalji = document.getElementById("posalji");
-
-        // Validavija imena (ovo je validacija regexom)
-        if (ime.value.length == 0 ) {
-            ime.style.backgroundColor="red";
-            document.getElementById("errorIme").style.display="block";
-            document.getElementById("errorIme").innerHTML="Ime ne smije ostati prazno.";
-            sveValidno = false;
-        }
-        else if (!ime.value.match(/^[a-zA-Z ČčĆćŽžŠšĐđ]+$/)) {
-            ime.style.backgroundColor="red";
-            document.getElementById("errorIme").style.display="block";
-            document.getElementById("errorIme").innerHTML="Ime mora sadrzavati samo slova.";
-            sveValidno = false;
-        }
-        else {
-            ime.style.backgroundColor="lightgreen";
-            document.getElementById("errorIme").style.display="none";
-        }
-
-        // Validacija maila
-        if (mail.value.length == 0 ) {
-            mail.style.backgroundColor="red";
-            document.getElementById("errorMail").style.display="block";
-            document.getElementById("errorMail").innerHTML="Mail ne smije ostati prazan.";
-            sveValidno = false;
-        }
-        else if (!mail.value.match(/^[a-z0-9_]+@[a-z]+\.[a-z][a-z]+$/)) {
-            mail.style.backgroundColor="red";
-            document.getElementById("errorMail").style.display="block";
-            document.getElementById("errorMail").innerHTML="Mail mora biti u tacnom formatu.";
-            sveValidno = false;
-        }
-        else {
-            mail.style.backgroundColor="lightgreen";
-            document.getElementById("errorMail").style.display="none";
-        }
-
-        // Validacija poruke (ovo je jedna validacija bez regexa)
-        if (poruka.value.length == 0) {
-            poruka.style.backgroundColor="red";
-            document.getElementById("errorPoruka").style.display="block";
-            sveValidno = false;
-        }
-        else {
-            poruka.style.backgroundColor="lightgreen";
-            document.getElementById("errorPoruka").style.display="none";
-        }
 
         if (true == sveValidno) {
             posalji.type = "submit";
@@ -75,5 +28,63 @@ $(document).ready(function(){
     function ucitanaSlika(e) {
         $('#kontakt_slikaKorisnika').attr('src', e.target.result);
     }
-
 });
+
+function validirajIme() {
+    var ime = document.getElementById("ime");
+
+    // Validavija imena (ovo je validacija regexom)
+    if (ime.value.length == 0 ) {
+        ime.style.backgroundColor="red";
+        document.getElementById("errorIme").style.display="block";
+        document.getElementById("errorIme").innerHTML="Ime ne smije ostati prazno.";
+        sveValidno = false;
+    }
+    else if (!ime.value.match(/^[a-zA-Z ČčĆćŽžŠšĐđ]+$/)) {
+        ime.style.backgroundColor="red";
+        document.getElementById("errorIme").style.display="block";
+        document.getElementById("errorIme").innerHTML="Ime mora sadrzavati samo slova.";
+        sveValidno = false;
+    }
+    else {
+        ime.style.backgroundColor="lightgreen";
+        document.getElementById("errorIme").style.display="none";
+    }
+}
+
+function validirajMail() {
+    var mail = document.getElementById("mail");
+
+    // Validacija maila
+    if (mail.value.length == 0 ) {
+        mail.style.backgroundColor="red";
+        document.getElementById("errorMail").style.display="block";
+        document.getElementById("errorMail").innerHTML="Mail ne smije ostati prazan.";
+        sveValidno = false;
+    }
+    else if (!mail.value.match(/^[a-z0-9_]+@[a-z]+\.[a-z][a-z]+$/)) {
+        mail.style.backgroundColor="red";
+        document.getElementById("errorMail").style.display="block";
+        document.getElementById("errorMail").innerHTML="Mail mora biti u tacnom formatu.";
+        sveValidno = false;
+    }
+    else {
+        mail.style.backgroundColor="lightgreen";
+        document.getElementById("errorMail").style.display="none";
+    }
+}
+
+function validirajPoruku() {
+    var poruka = document.getElementById("poruka");
+
+    // Validacija poruke (ovo je jedna validacija bez regexa)
+    if (poruka.value.length == 0) {
+        poruka.style.backgroundColor="red";
+        document.getElementById("errorPoruka").style.display="block";
+        sveValidno = false;
+    }
+    else {
+        poruka.style.backgroundColor="lightgreen";
+        document.getElementById("errorPoruka").style.display="none";
+    }
+}
